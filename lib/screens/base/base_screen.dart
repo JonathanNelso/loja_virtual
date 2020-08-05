fa6e7e9083904be0b225c24e839daaf70f07b90d
JonathanNelso <jonathanneves33@gmail.com>
2020-08-04 21:06:00 -0300

    Adicionado a custom_drawer (Barra de opções)

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_drawer/custom_drawer.dart';
 
 class baseScreen extends StatelessWidget {
+  
+  final PageController pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: pageController,
+      physics: const NeverScrollableScrollPhysics(),
       children: <Widget>[
+        Scaffold(
+          drawer: CustomDrawer(),
+          appBar: AppBar(
+            title: const Text('Home'),
+          ),
+        ),
         Container(color: Colors.red,),
         Container(color: Colors.yellow,),
         Container(color: Colors.green,),
